b2b524708b264535c4a16e146245b8a4ef83f845
Brandon Weaver <a7xncob@gmail.com>
2019-11-08 18:07:21 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
-
+class Book
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
+  
+  def initialize(title)
+    @this_title = title
+  end
+  
+  def title
+    return @this_title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
